b64a53d54685e29bf151b556efafbc149edab0b9
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-02-24 11:25:33 +0800

    feat: flatmap

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -1,5 +1,8 @@
 package com.learnkafkastreams.topology;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -23,10 +26,15 @@ public class GreetingsTopology {
     greetingStream.print(Printed.<String, String>toSysOut().withLabel("greetingStream"));
 
     KStream<String, String> modifiedStream =
-        greetingStream
-        .map((key, value) -> KeyValue.pair(key.toUpperCase(), value.toUpperCase()));
-        // .filter((key, value) -> value.length() > 5) // filter, filterNot
-        // .mapValues((readOnlyKey, value) -> value.toUpperCase());
+        greetingStream.flatMap(
+            (key, value) -> {
+              List<String> newValue = Arrays.asList(value.split(""));
+              List<KeyValue<String, String>> newKeyValue =
+                  newValue.stream()
+                      .map(t -> KeyValue.pair(key.toUpperCase(), t))
+                      .collect(Collectors.toList());
+              return newKeyValue;
+            });
     modifiedStream.print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
 
     modifiedStream.to(GREETINGS_UPPERCASE, Produced.with(Serdes.String(), Serdes.String()));
