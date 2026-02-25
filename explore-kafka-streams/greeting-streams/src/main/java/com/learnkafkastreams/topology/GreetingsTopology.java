3d41009731123c06f2163a0e244f5940f2fd3d81
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-02-25 17:25:06 +0800

    feat: Usage of Custom Serde in the Greeting App

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -1,5 +1,7 @@
 package com.learnkafkastreams.topology;
 
+import com.learnkafkastreams.domain.Greeting;
+import com.learnkafkastreams.serdes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -7,6 +9,7 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
 
 @Slf4j
 public class GreetingsTopology {
@@ -20,22 +23,43 @@ public class GreetingsTopology {
   public static Topology buildTopology() {
     StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-    KStream<String, String> greetingStream =
-        streamsBuilder.stream(GREETINGS, Consumed.with(Serdes.String(), Serdes.String()));
+    // KStream<String, String> mergedStream = getStringGreetingKStream(streamsBuilder);
+    KStream<String, Greeting> mergedStream = getCustomGreetingKStream(streamsBuilder);
+    KStream<String, Greeting> modifiedStream =
+        mergedStream.mapValues(
+            (readOnlyKey, value) ->
+                new Greeting(value.getMessage().toUpperCase(), value.getTimeStamp()));
 
-    KStream<String, String> greetingSpanishStream =
-        streamsBuilder.stream(GREETINGS_SPANISH, Consumed.with(Serdes.String(), Serdes.String()));
+    modifiedStream.print(Printed.<String, Greeting>toSysOut().withLabel("modifiedStream"));
+    modifiedStream.to(
+        GREETINGS_UPPERCASE, Produced.with(Serdes.String(), SerdesFactory.greetingSerdes()));
+
+    return streamsBuilder.build();
+  }
+
+  public static KStream<String, String> getStringGreetingKStream(StreamsBuilder streamsBuilder) {
+
+    KStream<String, String> greetingStream = streamsBuilder.stream(GREETINGS);
+    KStream<String, String> greetingSpanishStream = streamsBuilder.stream(GREETINGS_SPANISH);
 
     // merge 兩個 stream
     KStream<String, String> mergedStream = greetingStream.merge(greetingSpanishStream);
     mergedStream.print(Printed.<String, String>toSysOut().withLabel("mergedStream"));
+    return mergedStream;
+  }
 
-    KStream<String, String> modifiedStream =
-        mergedStream.mapValues((readOnlyKey, value) -> value.toUpperCase());
-    modifiedStream.print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
+  public static KStream<String, Greeting> getCustomGreetingKStream(StreamsBuilder streamsBuilder) {
 
-    modifiedStream.to(GREETINGS_UPPERCASE);
+    KStream<String, Greeting> greetingStream =
+        streamsBuilder.stream(
+            GREETINGS, Consumed.with(Serdes.String(), SerdesFactory.greetingSerdes()));
+    KStream<String, Greeting> greetingSpanishStream =
+        streamsBuilder.stream(
+            GREETINGS_SPANISH, Consumed.with(Serdes.String(), SerdesFactory.greetingSerdes()));
 
-    return streamsBuilder.build();
+    // merge 兩個 stream
+    KStream<String, Greeting> mergedStream = greetingStream.merge(greetingSpanishStream);
+    mergedStream.print(Printed.<String, Greeting>toSysOut().withLabel("mergedStream"));
+    return mergedStream;
   }
 }
