3c70457c660c92424f45788f4f44d52aaf32a304
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-02 11:28:38 +0800

    feat: Custom Production Error Handler

---
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/exceptionhandler/StreamsSerializationExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.learnkafkastreams.exceptionhandler;
+
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.streams.errors.ProductionExceptionHandler;
+
+@Slf4j
+public class StreamsSerializationExceptionHandler implements ProductionExceptionHandler {
+  @Override
+  public ProductionExceptionHandlerResponse handle(
+      ProducerRecord<byte[], byte[]> record, Exception exception) {
+    log.error(
+        "Serialization Exception is : {}, and the kafka record is : {}",
+        exception.getMessage(),
+        record,
+        exception);
+    return ProductionExceptionHandlerResponse.CONTINUE;
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    // TODO Auto-generated method stub
+
+  }
+}
--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -2,6 +2,7 @@ package com.learnkafkastreams.launcher;
 
 import com.learnkafkastreams.exceptionhandler.StreamDeserializationExceptionHandler;
 import com.learnkafkastreams.exceptionhandler.StreamsProcessorCustomErrorHandler;
+import com.learnkafkastreams.exceptionhandler.StreamsSerializationExceptionHandler;
 import com.learnkafkastreams.topology.GreetingsTopology;
 import java.util.List;
 import java.util.Properties;
@@ -27,6 +28,9 @@ public class GreetingsStreamApp {
     properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
     properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
     properties.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "2");
+    properties.put(
+        StreamsConfig.DEFAULT_PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG,
+        StreamsSerializationExceptionHandler.class);
     properties.put(
         StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
         StreamDeserializationExceptionHandler.class);
