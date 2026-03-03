ff652922be9646fcbb400ec5599054a8b055bac7
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-03 15:32:33 +0800

    feat: Aggregation Using "reduce" Operator

--- explore-kafka-streams/advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreAggregateOperatorsTopology.java
+++ explore-kafka-streams/advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreAggregateOperatorsTopology.java
@@ -21,8 +21,8 @@ public class ExploreAggregateOperatorsTopology {
     // 計算每個 key 底下的數量
     // [words-count-per-alphabet]: A, 9
     // [words-count-per-alphabet]: B, 6
-    // KGroupedStream<String, String> groupedStream =
-    //     inputStream.groupByKey(Grouped.with(Serdes.String(), Serdes.String()));
+    KGroupedStream<String, String> groupedStream =
+        inputStream.groupByKey(Grouped.with(Serdes.String(), Serdes.String()));
 
     // 計算每個字出現的次數
     // [words-count-per-alphabet]: Apple, 2
@@ -30,10 +30,12 @@ public class ExploreAggregateOperatorsTopology {
     // [words-count-per-alphabet]: Ambulance, 2
     // [words-count-per-alphabet]: Bus, 2
     // [words-count-per-alphabet]: Baby, 2
-    KGroupedStream<String, String> groupedStream =
-        inputStream.groupBy((key, value) -> value, Grouped.with(Serdes.String(), Serdes.String()));
+    // KGroupedStream<String, String> groupedStream =
+    //     inputStream.groupBy((key, value) -> value, Grouped.with(Serdes.String(),
+    // Serdes.String()));
 
-    exploreCount(groupedStream);
+    // exploreCount(groupedStream);
+    exploreReduce(groupedStream);
 
     return streamsBuilder.build();
   }
@@ -43,5 +45,16 @@ public class ExploreAggregateOperatorsTopology {
     countByAlphabet
         .toStream()
         .print(Printed.<String, Long>toSysOut().withLabel("words-count-per-alphabet"));
+  }
+
+  public static void exploreReduce(KGroupedStream<String, String> groupedStream) {
+    KTable<String, String> reducedStream =
+        groupedStream.reduce(
+            (value1, value2) -> {
+              log.info("value 1 : {}, value 2 : {}", value1, value2);
+              return value1.toUpperCase() + "-" + value2.toUpperCase();
+            });
+
+    reducedStream.toStream().print(Printed.<String, String>toSysOut().withLabel("reduced-words"));
   }
 }
