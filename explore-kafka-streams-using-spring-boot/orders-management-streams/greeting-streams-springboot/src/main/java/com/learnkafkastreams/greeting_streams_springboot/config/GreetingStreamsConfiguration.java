eb415364d7c27c3c75d28be354d18c3580ab7549
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-06 16:26:38 +0800

    feat: Handle Deserialization Errors Using Spring Specific Approach- Approach 3

--- explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/main/java/com/learnkafkastreams/greeting_streams_springboot/config/GreetingStreamsConfiguration.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/main/java/com/learnkafkastreams/greeting_streams_springboot/config/GreetingStreamsConfiguration.java
@@ -1,19 +1,56 @@
 package com.learnkafkastreams.greeting_streams_springboot.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.listener.ConsumerRecordRecoverer;
+import org.springframework.kafka.streams.RecoveringDeserializationExceptionHandler;
 import org.springframework.stereotype.Component;
 import com.learnkafkastreams.greeting_streams_springboot.topology.GreetingStreamsTopology;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.kafka.support.serializer.JsonSerde;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.learnkafkastreams.greeting_streams_springboot.domain.Greeting;
 
+@Slf4j
 @Component
 public class GreetingStreamsConfiguration {
+
+  @Autowired
+  KafkaProperties kafkaProperties;
+
+  @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+  public KafkaStreamsConfiguration kStreamsConfiguration(SslBundles sslBundles) {
+
+    var kafkaStreamProperties = kafkaProperties.buildStreamsProperties(sslBundles);
+
+    kafkaStreamProperties.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
+        RecoveringDeserializationExceptionHandler.class);
+    kafkaStreamProperties.put(
+        RecoveringDeserializationExceptionHandler.KSTREAM_DESERIALIZATION_RECOVERER, recoverer());
+
+    return new KafkaStreamsConfiguration(kafkaStreamProperties);
+  }
+
+  private ConsumerRecordRecoverer recoverer() {
+    return (consumerRecord, e) -> {
+
+      log.error("Exception is : {}, Failed Record : {}", consumerRecord, e.getMessage());
+
+    };
+  }
 
   @Bean
   public NewTopic greetingTopic() {
