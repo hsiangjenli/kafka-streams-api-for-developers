ac1fd438cdc644daff7c83eff71b0951e71eefce
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-06 11:24:25 +0800

    chore: change to maven

--- explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/main/java/com/learnkafkastreams/greeting_streams_springboot/GreetingStreamsSpringbootApplication.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/main/java/com/learnkafkastreams/greeting_streams_springboot/GreetingStreamsSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.learnkafkastreams;
+package com.learnkafkastreams.greeting_streams_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/test/java/com/learnkafkastreams/greeting_streams_springboot/GreetingStreamsSpringbootApplicationTests.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/greeting-streams-springboot/src/test/java/com/learnkafkastreams/greeting_streams_springboot/GreetingStreamsSpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learnkafkastreams;
+package com.learnkafkastreams.greeting_streams_springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
