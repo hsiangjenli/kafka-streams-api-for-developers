c1e3c17ec05fca442fa9ac78d5002c0e7ecc09a5
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-10 10:20:18 +0800

    feat: Unit Testing Greetings App - Testing Multiple Messages

--- explore-kafka-streams/orders-kafka-streams-app/src/test/java/com/learnkafkastreams/topology/GreetingsTopologyTest.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/test/java/com/learnkafkastreams/topology/GreetingsTopologyTest.java
@@ -3,7 +3,9 @@ package com.learnkafkastreams.topology;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.TestOutputTopic;
 import org.apache.kafka.streams.TopologyTestDriver;
@@ -53,7 +55,34 @@ public class GreetingsTopologyTest {
     assertEquals("GOOD MORNING", outputKeyValue.value.getMessage());
     assertNotNull(outputKeyValue.value.getTimeStamp());
 
+  }
 
+  @Test
+  void buildTopology_multiInput() {
+
+    // Given
+    var greeting1 = KeyValue.pair("GM", new Greeting("Good Morning", LocalDateTime.now()));
+    var greeting2 = KeyValue.pair("GN", new Greeting("Good Night", LocalDateTime.now()));
+
+    inputTopic.pipeKeyValueList(List.of(greeting1, greeting2));
+
+    // When
+    // Pass
+
+    // Then
+    var count = outputTopic.getQueueSize();
+    assertEquals(2, count);
+
+    var outputKeyValueList = outputTopic.readKeyValuesToList();
+    var output1 = outputKeyValueList.get(0);
+    var output2 = outputKeyValueList.get(1);
+    assertEquals("GM", output1.key);
+    assertEquals("GOOD MORNING", output1.value.getMessage());
+    assertNotNull(output1.value.getTimeStamp());
+
+    assertEquals("GN", output2.key);
+    assertEquals("GOOD NIGHT", output2.value.getMessage());
+    assertNotNull(output2.value.getTimeStamp());
 
   }
 
